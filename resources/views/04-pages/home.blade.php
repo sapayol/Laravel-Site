55e7fed940bafa178d44fe4ee674eca6d1742685
Dima Markus <dmi3000@gmail.com>
2015-07-02 01:34:24 -0400

    Add home page content

--- resources/views/04-pages/home.blade.php
+++ resources/views/04-pages/home.blade.php
@@ -5,12 +5,41 @@
 @stop
 
 @section('main')
-	{{-- <div class="large-12 medium-12 small-12 columns text-center"> --}}
+	<div class="large-12 medium-12 small-12 columns text-center hero-image-container">
 		<img class="hero-image" src="images/photos/jacket-full_body-profile.jpg" alt="">
 		<h1 class="hero-text">
-			<strong>Le<span>ather Jacke</span>ts</strong>
+			<span class="line2 thin">Luxury</span><br>
+			<span class="line1 thin">Tailor Made</span><br>
+			<strong class="line3">Leather Jackets</strong>
 		</h1>
-	{{-- </div> --}}
+	</div>
+
+	<section class="large-12 medium-12 small-12 columns">
+		<article>
+			<a href="/images/photos/tailor-supplies.jpg"><img class="sidekick-image" src="/images/photos/tailor-supplies.jpg" alt=""></a>
+			<h2 class="thin">Made-to-Measure</h2>
+			<p>Your jacket is made exclusively for you, based on your measurements. It will fit you perfectly.</p>
+		</article>
+
+		<article>
+			<a href="/images/photos/tailor-at-work.jpg"><img class="sidekick-image" src="/images/photos/tailor-at-work.jpg" alt=""></a>
+			<h2 class="thin">Master Craftsmanship</h2>
+			<p>Each piece is made at a select workshop, where the same craftsmen and -women have been perfecting their art together for centuries.</p>
+		</article>
+
+		<article>
+			<a href="/images/photos/zipper-brown.jpg"><img class="sidekick-image" src="/images/photos/zipper-brown.jpg" alt=""></a>
+			<h2 class="thin">Finest Materials</h2>
+			<p>From the most precious leather down to the zippers and buttons, we obsess over the quality of the materials we use.</p>
+		</article>
+
+		<article>
+			<a href="/images/photos/zipper-brown.jpg"><img class="sidekick-image" src="/images/photos/tannery.jpg" alt=""></a>
+			<h2 class="thin">Respect For People and Nature</h2>
+			<p>A deep concern for work conditions, our environment, and animals guides every decision we take.</p>
+		</article>
+	</section>
+
 @stop
 
 @section('footer')
