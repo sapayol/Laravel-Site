8bc715128a33b94795aa84a7ad95c928cbc334a9
Dima Markus <dmi3000@gmail.com>
2015-07-02 01:33:56 -0400

    Change nav item

--- resources/views/01-molecules/navigation/primary-nav.blade.php
+++ resources/views/01-molecules/navigation/primary-nav.blade.php
@@ -1,14 +1,17 @@
 <nav class="primary-nav" role="navigation">
   <a href="/home" class="logo"><img src="/images/logo-black.svg" alt="Sapayol Logo"></a></li>
   <ul class="no-bullet">
-    <li class="{!! Request::is('jackets') ? 'active' : '' !!}">
-      <a href="/jackets">Our Jackets</a>
-    </li>
+    {{-- <li class="{!! Request::is('jackets') ? 'active' : '' !!}"> --}}
+      {{-- <a href="/jackets">Our Jackets</a> --}}
+    {{-- </li> --}}
     <li class="{!! Request::is('who-we-are') ? 'active' : '' !!}">
       <a href="/who-we-are">Who We Are</a>
     </li>
     <li class="{!! Request::is('how-it-works') ? 'active' : '' !!}">
       <a href="/how-it-works">How It Works</a>
+    </li>
+    <li class="{!! Request::is('jackets/bomber') ? 'active' : '' !!}">
+      <a href="/jackets/bomber">Our Signature Jacket</a>
     </li>
   </ul>
 </nav>
