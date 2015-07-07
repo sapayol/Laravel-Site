cc1d2bdedea2bb9a0880e17124c7cbc64370746a
Dima Markus <dmi3000@gmail.com>
2015-07-07 18:12:24 -0400

    Split apart the back-end controllers

--- app/Http/Controllers/JacketsController.php
+++ app/Http/Controllers/JacketsController.php
@@ -25,7 +25,7 @@ class JacketsController extends Controller {
 		$jacket = Jacket::where('model', '=', $model)->first();
 		$measurements = Measurement::where('type', '=', 'standard')->get();
 
-		return view('04-pages.jackets.look2', ['jacket' => $jacket, 'measurements' => $measurements]);
+		return view('04-pages.jackets.look', ['jacket' => $jacket, 'measurements' => $measurements]);
 	}
 
 	public function fit($model, Request $request)
@@ -41,31 +41,6 @@ class JacketsController extends Controller {
 			'lining_color'   => $request->lining,
 			'hardware_color' => $request->hardware
 		]);
-	}
-
-	public function getCheckout($model, Request $request)
-	{
-		$jacket = Jacket::where('model', '=', $model)->first();
-
-		return view('04-pages.jackets.checkout', [
-			'jacket'         => $jacket,
-			'leather_type'   => $request->leather_type,
-			'leather_color'  => $request->leather_color,
-			'lining_color'   => $request->lining_color,
-			'hardware_color' => $request->hardware_color
-		]);
-	}
-
-	public function postCheckout()
-	{
-		return redirect()->route('jackets.checkout-complete');
-	}
-
-	public function checkoutComplete($model)
-	{
-		$jacket = Jacket::where('model', '=', $model)->first();
-
-		return view('04-pages.jackets.checkout-complete', ['jacket' => $jacket]);
 	}
 
 }
