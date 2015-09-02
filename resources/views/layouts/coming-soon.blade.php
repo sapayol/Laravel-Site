e19dfa2e2969219fd0d3086ab7354fff1205157f
Dima Markus <dmi3000@gmail.com>
2015-09-01 23:26:06 -0400

    Update coming soon page

--- resources/views/layouts/coming-soon.blade.php
+++ resources/views/layouts/coming-soon.blade.php
@@ -1,7 +1,7 @@
-{!! view('partials.global._head') !!}
+@include('partials.global._head')
 
 <main class="row">
 	@yield('main')
 </main>
-<h1>yep</h1>
-{!! view('partials.global._foot') !!}
+
+@include('partials.global._foot')
