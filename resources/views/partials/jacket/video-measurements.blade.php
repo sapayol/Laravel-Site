c596543fefbae528c1b2ec9cbfa80ae9811e7962
Dima Markus <dmi3000@gmail.com>
2015-09-08 14:33:14 -0400

    Update font weights and measurement chart

--- resources/views/partials/jacket/video-measurements.blade.php
+++ resources/views/partials/jacket/video-measurements.blade.php
@@ -1,9 +1,9 @@
 
-<div class="large-12 medium-12 small-12 columns">
+<div class="large-12 medium-12 small-12 columns text-center">
 	<a class="video-caption underlined" ng-show="!measurementsVisible" ng-click="measurementsVisible = !measurementsVisible">Measurements Seen In the Video </a>
 </div>
 
-<section ng-show="measurementsVisible">
+<section ng-show="measurementsVisible" class="animated slideInDown">
 	@yield('measurement_data')
 		<ul ng-if="metricUnits" class="no-bullet value-list tight-list large-6 medium-6 small-6 columns">
 			<li><strong><small>Model Measurements</small></strong></li>
@@ -29,19 +29,8 @@
 				<li><small class="list-key">{{{ ucwords(str_replace('_', ' ', $key)) }}}</small> {{{ $value !== '' ? round($value  / 2.54, 1) : '--' }}} <small>{{{ $value !== '' ? 'in' : '' }}}</small></li>
 				@endforeach
 		</ul>
-	<div class="small-12 medium-12 large-12 columns">
-		<p>Model wears a size {{{ $model_size }}} with the following adjustments:</p>
-		<ul ng-if="metricUnits" class="no-bullet value-list tight-list left">
-			@foreach ($video_adjustments as $key => $value)
-				<li><small class="list-key">{{{ ucwords(str_replace('_', ' ', $key)) }}}</small> {{{ $value }}} <small>cm</small></li>
-			@endforeach
-		</ul>
-		<ul ng-if="!metricUnits" class="no-bullet value-list tight-list left">
-			@foreach ($video_adjustments as $key => $value)
-				<li><small class="list-key">{{{ ucwords(str_replace('_', ' ', $key)) }}}</small> {{{ round($value / 2.54, 1) }}} <small>in</small></li>
-			@endforeach
-		</ul>
-		<button ng-click="metricUnits = !metricUnits" class="right button tiny radius">cm / in</button>
-		<a ng-show="measurementsVisible" class="right" ng-click="measurementsVisible = !measurementsVisible">Hide measurements</a>
+	<div class="large-12 medium-12 small-12 columns">
+		<button ng-click="metricUnits = !metricUnits" class="right button tiny">cm / in</button>
+		<a ng-show="measurementsVisible" class="left underlined" ng-click="measurementsVisible = !measurementsVisible">Hide measurements</a>
 	</div>
 </section>
