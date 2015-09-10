d418807b120a62c16688f455c2ee885ba5239f3a
Dima Markus <dmi3000@gmail.com>
2015-09-10 14:37:29 -0400

    Update jackets page

--- resources/views/pages/jackets/index.blade.php
+++ resources/views/pages/jackets/index.blade.php
@@ -9,27 +9,28 @@
 @stop
 
 @section('main')
-<div class="large-12 medium-12 small-12 columns">
-	<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-		@foreach ($jackets as $jacket)
-		  <li class="text-center">
-				@if ($jacket->active)
-					<a href="/jackets/{{{ $jacket->model }}}">
-						<img class="responsive-image" src="/images/photos/jackets/{{{ $jacket->model }}}/thumbnail.jpg">
-					</a>
-					<h2>
+	<article class="large-12 medium-12 small-12 columns">
+		<p>Choose the model you would like to customize and have tailor-made.</p>
+		<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
+			@foreach ($jackets as $jacket)
+			  <li class="text-center">
+					@if ($jacket->active)
 						<a href="/jackets/{{{ $jacket->model }}}">
-							{{{ $jacket->name }}}
+							<img class="responsive-image" src="/images/photos/jackets/{{{ $jacket->model }}}/thumbnail.jpg">
 						</a>
-					</h2>
-				@else
-					<img class="jacket-placeholder" src="/images/photos/jackets/{{{ $jacket->model }}}/{{{ $jacket->model }}}.svg">
-					<h2>{{{ $jacket->name }}}</h2>
-					<h3><em>Coming Soon</em></h3>
-				@endif
-		  </li>
-		@endforeach
-	</ul>
-</div>
-
+						<h2>
+							<a href="/jackets/{{{ $jacket->model }}}">
+								{{{ $jacket->name }}}
+							</a>
+						</h2>
+					@else
+						<img class="jacket-placeholder" src="/images/photos/jackets/{{{ $jacket->model }}}/{{{ $jacket->model }}}.svg">
+						<h2>{{{ $jacket->name }}}</h2>
+						<h3><em>Coming Soon</em></h3>
+						<br>
+					@endif
+			  </li>
+			@endforeach
+		</ul>
+	</article>
 @stop
