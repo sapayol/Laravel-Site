05f49b1880ac5d537ef62185635de412d3dd16e9
Dima Markus <dmi3000@gmail.com>
2015-10-05 20:40:16 -0400

    Fix terms page link

--- resources/views/pages/info/terms.blade.php
+++ resources/views/pages/info/terms.blade.php
@@ -51,8 +51,8 @@
 	</nav>
 
 	<header class="large-12 medium-12 small-12 columns terms" id="overview">
-		<h2>OVERVIEW</h2>
-		<a href="#terms-nav" class under-heading-link="underlined">Back to Top</a>
+		<h2>OVERVIEW</h2><br>
+		<a href="#terms-nav" class="under-heading-link underlined">Back to Top</a>
 		<p>This website is operated by SAPA YOL INC. Throughout the site, the terms “we”, “us” and “our” refer to SAPA YOL INC. SAPA YOL INC offers this website, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.</p>
 		<p>By visiting our site and/ or purchasing something from us, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.</p>
 		<p>Please read these Terms of Service carefully before accessing or using our website. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.</p>
