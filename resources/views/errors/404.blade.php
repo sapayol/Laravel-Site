9fe910146c3aa447a44a1dacd46eb02513a1cd36
Dima Markus <dmi3000@gmail.com>
2015-10-05 23:51:49 -0400

    Update 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,8 @@
 
 	<section class="large-12 medium-12 small-12 columns">
 		<h1>Page not found.</h1>
-		<p>Looks like something went wrong. Please make sure you typed the correct URL into the address bar.</p>
-		<p>Did you discover a problem during your browsing experience? Please let us know so we can help you avoid it in the future.</p>
+		<p>Looks like something went wrong. Please make sure the URL in the address bar is correct.</p>
+		<p>Did you discover a problem on our site? Please let us know so we can help you avoid it in the future.</p>
 		<div class="text-center">
 			<a class="underlined" href="mailto:contact@sapayol.com" title="">Contact Us</a>
 			<br><br>
