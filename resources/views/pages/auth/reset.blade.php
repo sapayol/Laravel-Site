4dcc5fb563b75184520d2704eb81161fa6b91211
Dima Markus <dmi3000@gmail.com>
2015-10-07 18:51:39 -0400

    Update reset page

--- resources/views/pages/auth/reset.blade.php
+++ resources/views/pages/auth/reset.blade.php
@@ -2,59 +2,39 @@
 
 
 @section('main')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<input type="hidden" name="token" value="{{ $token }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Reset Password
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+	<section class="large-12 medium-12 small-12 columns" ng-controller="authCtrl">
+		@if (count($errors) > 0)
+			<div ng-hide="hideAlert" data-alert class="alert-box alert floating animated shake" ng-init="hideAlert = false">
+				<ul class="no-bullet">
+					@foreach ($errors->all() as $error)
+						<li><small>{{ $error }}</small></li>
+					@endforeach
+				</ul>
+			  <a href="#" class="close" ng-click="hideAlert = true">&times;</a>
 			</div>
-		</div>
-	</div>
-</div>
+		@endif
+		<form role="form" method="POST" action="{{ url('/auth/login') }}" name="userInfoForm">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<label for="email">Email Address
+				<span class="right alert" ng-if="showUserErorrs && userInfoForm.email.$invalid">Invalid email</span>
+				<input name="email" type="email" value="{{ old('email') }}" ng-model="user.email" ng-required="true" ng-disabled="userInfoSubmitted">
+			</label>
+
+			<label for="name" ng-hide="resetMode">Password
+				<span class="right alert" ng-if="showUserErorrs && userInfoForm.name.$invalid">Required</span>
+				<input name="password" type="password" ng-model="user.password" ng-required="true" ng-disabled="userInfoSubmitted">
+			</label>
+
+			<label for="name" ng-hide="resetMode">Confirm Password
+				<span class="right alert" ng-if="showUserErorrs && userInfoForm.name.$invalid">Required</span>
+				<input name="password_confirmation" type="password" ng-model="user.password_confirmation" ng-required="true" ng-disabled="userInfoSubmitted">
+			</label>
+
+			<div class="text-center">
+				<input class="button expand animated slideInDown" type="submit" value="Reset Password">
+				<a ng-click="resetMode = false" class="underlined under-button-link">Try logging in again</a>
+				<br>
+			</div>
+		</form>
+	</section>
 @endsection
