ce85d4b4f6dfaa59218c8a8bf13d2533f0b24b3b
Dima Markus <dmi3000@gmail.com>
2015-10-19 12:56:09 -0400

    Fix user profile

--- resources/views/pages/users/show.blade.php
+++ resources/views/pages/users/show.blade.php
@@ -17,35 +17,39 @@
 		<h4>{{{ $user->name }}}</h4>
 		<h5>{{{ $user->email }}}</h5>
 	</section>
-	<section class="large-12 medium-12 small-12 columns">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Status</th>
-					<th>Measurements</th>
-					<th>&nbsp;</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($user->orders as $order)
+	@if ($user->orders)
+		<section class="large-12 medium-12 small-12 columns">
+			<h2>Orders</h2>
+			<table class="table">
+				<thead>
 					<tr>
-						<td>{{{ $order->id }}} </td>
-						<td>{{{ $order->status }}} </td>
-						<td>
-							@if ($order->userMeasurements)
-								@foreach ($order->userMeasurements->completed() as $measurement)
-									<small>{{{ $measurement }}} <strong>{{{ $order->userMeasurements->$measurement }}}</strong></small><br>
-								@endforeach
-							@else
-								<small>N/A</small>
-							@endif
-						</td>
-						<td><a href="{{{ route('orders.show', $order->id) }}}" title="">View</a></td>
+						<th>#</th>
+						<th>Status</th>
+						<th>Measurements</th>
+						<th>&nbsp;</th>
 					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</section>
+				</thead>
+				<tbody>
+					@foreach ($user->orders as $order)
+						<tr>
+							<td>{{{ $order->id }}} </td>
+							<td>{{{ $order->status }}} </td>
+							<td>
+								@if ($order->userMeasurements && $order->userMeasurements->completed())
+									@foreach ($order->userMeasurements->completed() as $measurement)
+										<small>{{{ $measurement }}} <strong>{{{ $order->userMeasurements->$measurement }}}</strong></small><br>
+									@endforeach
+								@else
+									<small>N/A</small>
+								@endif
+							</td>
+							<td><a href="{{{ route('orders.show', $order->id) }}}" title="">View</a></td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</section>
+	@endif
+
 @stop
 
