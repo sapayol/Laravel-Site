447692a43d97caa37bbccb93ad09388a8547bef5
Dima Markus <dmi3000@gmail.com>
2015-10-30 02:11:10 -0400

    Add responsiveness to how it works page

--- resources/views/pages/info/how-it-works.blade.php
+++ resources/views/pages/info/how-it-works.blade.php
@@ -5,51 +5,73 @@
 @stop
 
 @section('main')
-	<section class="large-12 medium-12 small-12 columns">
+	<article class="large-6 large-centered medium-8 medium-centered small-12 columns">
 		<p>Every SAPAYOL leather jacket is made for you, and no one else. We tailor each piece according to your measurements and customizations and then ship it directly from the atelier to your door.</p>
-	</section>
-
-	<section class="large-12 medium-12 small-12 columns">
-		<h2>Order Process</h2>
-		<h3>1. Choose your model</h3>
-		<h3>2. Design your look</h3>
-		<p>For every model, you can choose hardware and lining colors.</p>
-		<h3>3. Create an account or log in</h3>
-		<p>An email address and password lets us save the design choices you just made and the body measurements you&rsquo;re about to enter.</p>
-		<h3>4. Provide measurements</h3>
-		<p>With the help of our video instructions, you can add your measurements to your order.</p>
-		<h3>5. Buy</h3>
-		<p>We accept all major credit cards.</p>
-		<h3>6. Discuss measurements</h3>
-		<p>Within a day, we will get in touch with you by phone or email and discuss your measurements. We&rsquo;ll make sure you&rsquo;ll get the exact fit you want.</p>
-		<h3>7. Production starts</h3>
-		<h3>8. Direct shipment to your door</h3>
-		<p>You will be able to track your order via express shipping.</p>
-	</section>
-
-	<section class="large-12 medium-12 small-12 columns">
-		<article id="return-policy">
-			<h3>Returns</h3>
-			<p>Because each leather jacket is made exclusively for you, <em>all sales are final</em>.</p>
-			<p>We take extra care in discussing your measurements and any other questions with you before production to make sure you will be satisfied.</p>
-			<p>The only exception to this rule is if your jacket deviates significantly from the measurements we agreed on. After we&rsquo;ve confirmed the issue with you, you can send us the unused jacket in the same condition that you received it. We will remake the jacket for free and pay for shipping.</p>
-			<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
-		</article>
-		<article>
-			<h3>Remakes</h3>
-			<p>In the rare case of defects in materials or workmanship, contact us within 48 hours of receiving your jacket. If it can be fixed, we will fix it 	for free and pay for shipping. If it can&rsquo;t be fixed, we will remake the jacket for free.</p>
-			<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
-		</article>
-		<article>
-			<h3>Warranty</h3>
-			<p>All our jackets come with a 1 year warranty against defects in materials or workmanship. Contact us to fix any issues. We will pay for shipping.</p>
-			<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
-		</article>
-		<article>
-			<h3>Repairs</h3>
-			<p>Should you get any tears, abrasions or holes in your jacket, you can send it to us at any time and we will repair it by replacing the damaged part with a new one of the same leather quality. We will not charge for work, all you will have to cover are shipping costs and materials.</p>
-			<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
-		</article>
-		<a href="/jackets" class="button call-to-action expand">See Our Jackets</a>
-	</section>
+		<ol>
+			<br>
+			<h2 class="text-center">Order Process</h2>
+			<li>
+				<h3>Choose a model</h3>
+				<p></p>
+			</li>
+			<li>
+				<h3>Design your look</h3>
+				<p>For every model, you can choose hardware and lining colors.</p>
+			</li>
+			<li>
+				<h3>Create an account or log in</h3>
+				<p>An email address and password lets us save the design choices you just made and the body measurements you&rsquo;re about to enter.</p>
+			</li>
+			<li>
+				<h3>Provide measurements</h3>
+				<p>With the help of our video instructions, you can add your measurements to your order.</p>
+			</li>
+			<li>
+				<h3>Buy</h3>
+				<p>We accept all major credit cards.</p>
+			</li>
+			<li>
+				<h3>Discuss measurements</h3>
+				<p>Within a day, we will get in touch with you by phone or email and discuss your measurements. We&rsquo;ll make sure you&rsquo;ll get the exact fit you want.</p>
+			</li>
+			<li>
+				<h3>Production starts</h3>
+				<p></p>
+			</li>
+			<li>
+				<h3>Direct shipment to your door</h3>
+				<p>You will be able to track your order via express shipping.</p>
+			</li>
+		</ol>
+		<div class="text-center">
+			<a href="/jackets" class="button call-to-action expand-on-small">Start An Order Now</a>
+		</div>
+	</article>
+	<hr>
+	<article class="large-12 medium-12 small-12 columns">
+		<ul class="large-block-grid-2">
+			<li id="return-policy">
+				<h3>Returns</h3>
+				<p>Because each leather jacket is made exclusively for you, <em>all sales are final</em>.</p>
+				<p>We take extra care in discussing your measurements and any other questions with you before production to make sure you will be satisfied.</p>
+				<p>The only exception to this rule is if your jacket deviates significantly from the measurements we agreed on. After we&rsquo;ve confirmed the issue with you, you can send us the unused jacket in the same condition that you received it. We will remake the jacket for free and pay for shipping.</p>
+				<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
+			</li>
+			<li>
+				<h3>Remakes</h3>
+				<p>In the rare case of defects in materials or workmanship, contact us within 48 hours of receiving your jacket. If it can be fixed, we will fix it 	for free and pay for shipping. If it can&rsquo;t be fixed, we will remake the jacket for free.</p>
+				<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
+			</li>
+			<li>
+				<h3>Warranty</h3>
+				<p>All our jackets come with a 1 year warranty against defects in materials or workmanship. Contact us to fix any issues. We will pay for shipping.</p>
+				<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
+			</li>
+			<li>
+				<h3>Repairs</h3>
+				<p>Should you get any tears, abrasions or holes in your jacket, you can send it to us at any time and we will repair it by replacing the damaged part with a new one of the same leather quality. We will not charge for work, all you will have to cover are shipping costs and materials.</p>
+				<a class="underlined" href="mailto:contact@sapayol.com">contact@sapayol.com</a>
+			</li>
+		</ul>
+	</article>
 @stop
