f5945623fd250b6ce0f1e834b6ab9a650ec8b44e
Dima Markus <dmi3000@gmail.com>
2015-11-04 21:27:28 -0500

    Fix main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,12 +12,25 @@ abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
 
+    public $request;
+
+    public $current_uri;
+
+    public $current_user;
+
+    public $action;
+
     public function __construct(Request $request)
     {
-    		$this->request = $request;
-				$uri = $this->request->route()->getUri();
-				$current_user = Auth::user();
-				$action = $this->request->route() !== null ? $this->request->route()->getAction()['as'] : null;
-        view()->share(['action' => $action, 'current_uri' => $uri, 'current_user' => $current_user]);
+				$this->request      = $request;
+				$this->uri          = $this->request->route()->getUri();
+				$this->current_user = Auth::user();
+				$this->action       = null;
+
+				if ($this->request->route()  && array_key_exists('as', $this->request->route()->getAction())) {
+					$this->action = $this->request->route()->getAction()['as'];
+				}
+
+        view()->share(['action' => $this->action, 'current_uri' => $this->uri, 'current_user' => $this->current_user]);
     }
 }
