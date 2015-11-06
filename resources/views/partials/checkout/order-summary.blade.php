c61deb2a3d17bdabdad66998dcafd803051bccf5
Dima Markus <dmi3000@gmail.com>
2015-11-06 02:48:46 -0500

    Adjust note on checkout

--- resources/views/partials/checkout/order-summary.blade.php
+++ resources/views/partials/checkout/order-summary.blade.php
@@ -4,6 +4,7 @@
 	<a href="/jackets/{{{ $order->jacket->model }}}/look" class="right underlined">Change</a>
 	<div class="clearfix"></div>
 	<ul class="no-bullet value-list">
+		<li><small class="list-key">Order Total </small><strong>{{{ $order->total }}}	</strong></li>
 		<li><small class="list-key">Leather Type </small><strong>{{{ ucfirst($order->leather_type()->name)  }}}	</strong></li>
 		<li><small class="list-key">Leather Color </small><strong>{{{ ucfirst($order->leather_color()->name) }}}	</strong></li>
 		<li><small class="list-key">Lining Color </small><strong>{{{ ucfirst($order->lining_color()->name) }}}	</strong></li>
@@ -17,25 +18,31 @@
 		@foreach ($measurements as $measurement)
 			@if ($measurement == 'note')
 				<li><br></li>
+				<li class="note-row">
+					<small class="left list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
+					<span class="right list-value"><em>{{{ $order->bodyMeasurements->$measurement }}}</em></span>
+					<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title="">
+						<span>{{{ $order->bodyMeasurements->$measurement ? 'Change' : 'Add' }}}</span>
+					</a>
+				</li>
+			@else
+				<li>
+					<small class="list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
+					@if ($order->bodyMeasurements->$measurement)
+							<span class="list-value">
+							@if ($order->bodyMeasurements->units == 'in')
+								<strong decimal-to-fraction="{{{ $order->bodyMeasurements->$measurement }}}">{{{ $order->bodyMeasurements->$measurement }}}</strong> "
+							@else
+								<strong>{{{ $order->bodyMeasurements->$measurement != round($order->bodyMeasurements->$measurement) ?  round($order->bodyMeasurements->$measurement, 1) : round($order->bodyMeasurements->$measurement) }}}</strong> cm
+							@endif
+						</span>
+						<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Change</span></a>
+					@else
+						<span class="list-value"></span>
+						<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Add</span></a>
+					@endif
+				</li>
 			@endif
-			<li>
-				<small class="list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
-				@if ($order->bodyMeasurements->$measurement)
-					<span class="list-value">
-						@if ($measurement == 'note')
-							<em>{{{ $order->bodyMeasurements->$measurement }}}</em>
-						@elseif ($order->bodyMeasurements->units == 'in')
-							<strong decimal-to-fraction="{{{ $order->bodyMeasurements->$measurement }}}">{{{ $order->bodyMeasurements->$measurement }}}</strong> "
-						@else
-							<strong>{{{ $order->bodyMeasurements->$measurement != round($order->bodyMeasurements->$measurement) ?  round($order->bodyMeasurements->$measurement, 1) : round($order->bodyMeasurements->$measurement) }}}</strong> cm
-						@endif
-					</span>
-					<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Change</span></a>
-				@else
-					<span class="list-value"></span>
-					<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Add</span></a>
-				@endif
-			</li>
 		@endforeach
 	</ul>
 
