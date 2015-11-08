ee3d4c591d7f48b2536d5ae1cae55051eeb770ba
Dima Markus <dmi3000@gmail.com>
2015-11-08 18:01:32 -0500

    Rearrange checkout screen responsively

--- resources/views/pages/orders/checkout.blade.php
+++ resources/views/pages/orders/checkout.blade.php
@@ -10,26 +10,48 @@
 
 @section('main')
 
-<div class="small-12 medium-8 large-5 large-offset-1 medium-centered large-uncentered columns">
+<div class="small-12 medium-8 large-6 large-offset-3 columns">
 	<section class="checkout-form" id="address">
 		<h2 class="left"><small>Please enter your</small> <br>Shipping Address</h2>
 		<a ng-click="changeAddress()" ng-show="addressSubmitted" class="right underlined">Change</a>
 		<div class="clearfix"></div>
 		@include('partials.checkout.address-form')
 	</section>
-
 	<section class="checkout-form animated fadeIn" ng-show="addressSubmitted" id="payment-info">
+		<h2>Order Total</h2>
+		<ul class="no-bullet value-list tight-list">
+			<li>
+				<small class="list-key">{{{ $order->jacket->model }}}</small>
+				<span class="list-value"><small>$ </small>&nbsp;{{{ $order->jacket->price }}}</span>
+			</li>
+			<li>
+				<small class="list-key">Shipping</small>
+				<span class="list-value"><small>$ </small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.00 <small><em>Free shipping worldwide</em></small></span>
+			</li>
+			<li>
+				<small class="list-key">Taxes</small>
+				<span class="list-value"><small>$ </small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.00 <small><em>Taxes and duties included</em></small></span>
+			</li>
+			<li>
+				<small class="list-key"><strong>Order Total</strong></small>
+				<strong class="list-value"><small>$ </small>{{{ $order->total }}}</strong>
+			</li>
+		</ul>
+		<br>
 		<h2 class="left"><small>Please enter your</small> <br>Payment Info</h2>
 		<a ng-show="paymentInfoSubmitted" ng-click="changePaymentInfo()" class="right underlined">Change</a>
 		<div class="clearfix"></div>
 		@include('partials.checkout.payment-info-form')
 	</section>
 </div>
+<div class="clearfix"></div>
 
-
-<section class="small-12 medium-8 large-4 large-offset-1 medium-centered large-uncentered columns" ng-show="paymentInfoSubmitted" id="order-summary">
-	@include('partials.checkout.order-summary')
-</section>
+<div class="small-12 medium-8 large-6 large-offset-3 columns" ng-show="paymentInfoSubmitted" id="order-summary">
+	<section class="stick-to-bottom">
+		<h2>Your Jacket</h2>
+		@include('partials.checkout.order-summary')
+	</section>
+</div>
 
 <div class="clearfix"></div>
 <section class="small-12  medium-8 large-6 medium-centered columns text-center" ng-show="paymentInfoSubmitted">
--- resources/views/partials/checkout/order-summary.blade.php
+++ resources/views/partials/checkout/order-summary.blade.php
@@ -1,48 +1,53 @@
-	<h2><br class="show-for-large-up">Your {{{ $order->jacket->name }}}</h2>
 	<img class="customization-image" src="/images/photos/jackets/{{{ $order->jacket->model }}}/hardware-{{{ $order->hardware_color()->name }}}.jpg">
-	<h3 class="thin left">Look</h3>
-	<a href="/jackets/{{{ $order->jacket->model }}}/look" class="right underlined">Change</a>
-	<div class="clearfix"></div>
-	<ul class="no-bullet value-list">
-		<li><small class="list-key">Order Total </small><strong>{{{ $order->total }}}	</strong></li>
-		<li><small class="list-key">Leather Type </small><strong>{{{ ucfirst($order->leather_type()->name)  }}}	</strong></li>
-		<li><small class="list-key">Leather Color </small><strong>{{{ ucfirst($order->leather_color()->name) }}}	</strong></li>
-		<li><small class="list-key">Lining Color </small><strong>{{{ ucfirst($order->lining_color()->name) }}}	</strong></li>
-		<li><small class="list-key">Hardware Color </small><strong>{{{ ucfirst($order->hardware_color()->name) }}}	</strong></li>
-	</ul>
 
-	<h3 class="thin left">Fit</h3>
-	<div class="clearfix"></div>
-	<ul class="no-bullet value-list">
-		<?php $measurements = ['height', 'half_shoulder', 'back_width', 'chest', 'stomach', 'back_length', 'waist', 'arm', 'biceps', 'note'];  ?>
-		@foreach ($measurements as $measurement)
-			@if ($measurement == 'note')
-				<li><br></li>
-				<li class="note-row">
-					<small class="left list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
-					<span class="right list-value"><em>{{{ $order->bodyMeasurements->$measurement }}}</em></span>
-					<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title="">
-						<span>{{{ $order->bodyMeasurements->$measurement ? 'Change' : 'Add' }}}</span>
-					</a>
-				</li>
-			@else
-				<li>
-					<small class="list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
-					@if ($order->bodyMeasurements->$measurement)
-							<span class="list-value">
-							@if ($order->bodyMeasurements->units == 'in')
-								<strong decimal-to-fraction="{{{ $order->bodyMeasurements->$measurement }}}">{{{ $order->bodyMeasurements->$measurement }}}</strong> "
-							@else
-								<strong>{{{ $order->bodyMeasurements->$measurement != round($order->bodyMeasurements->$measurement) ?  round($order->bodyMeasurements->$measurement, 1) : round($order->bodyMeasurements->$measurement) }}}</strong> cm
-							@endif
-						</span>
-						<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Change</span></a>
-					@else
-						<span class="list-value"></span>
-						<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Add</span></a>
-					@endif
-				</li>
-			@endif
-		@endforeach
-	</ul>
+	<section class="large-6 medium-8 small-12 columns">
+		<h3 class="thin left">Look</h3>
+		<a href="/jackets/{{{ $order->jacket->model }}}/look" class="right underlined">Change</a>
+		<div class="clearfix"></div>
+		<ul class="no-bullet value-list">
+			<li><small class="list-key">Model</small><strong>{{{ ucfirst($order->jacket->name)  }}}	</strong></li>
+			<li><small class="list-key">Leather Type </small><strong>{{{ ucfirst($order->leather_type()->name)  }}}	</strong></li>
+			<li><small class="list-key">Leather Color </small><strong>{{{ ucfirst($order->leather_color()->name) }}}	</strong></li>
+			<li><small class="list-key">Lining Color </small><strong>{{{ ucfirst($order->lining_color()->name) }}}	</strong></li>
+			<li><small class="list-key">Hardware Color </small><strong>{{{ ucfirst($order->hardware_color()->name) }}}	</strong></li>
+		</ul>
+	</section>
+
+
+	<section class="large-6 medium-8 small-12 columns">
+		<h3 class="thin left">Fit</h3>
+		<div class="clearfix"></div>
+		<ul class="no-bullet value-list">
+			<?php $measurements = ['height', 'half_shoulder', 'back_width', 'chest', 'stomach', 'back_length', 'waist', 'arm', 'biceps', 'note'];  ?>
+			@foreach ($measurements as $measurement)
+				@if ($measurement == 'note')
+					<li><br></li>
+					<li class="note-row">
+						<small class="left list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
+						<span class="right list-value"><em>{{{ $order->bodyMeasurements->$measurement }}}</em></span>
+						<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title="">
+							<span>{{{ $order->bodyMeasurements->$measurement ? 'Change' : 'Add' }}}</span>
+						</a>
+					</li>
+				@else
+					<li>
+						<small class="list-key">{{{ ucwords(str_replace('_', ' ', $measurement)) }}}</small>
+						@if ($order->bodyMeasurements->$measurement)
+								<span class="list-value">
+								@if ($order->bodyMeasurements->units == 'in')
+									<strong decimal-to-fraction="{{{ $order->bodyMeasurements->$measurement }}}">{{{ $order->bodyMeasurements->$measurement }}}</strong> "
+								@else
+									<strong>{{{ $order->bodyMeasurements->$measurement != round($order->bodyMeasurements->$measurement) ?  round($order->bodyMeasurements->$measurement, 1) : round($order->bodyMeasurements->$measurement) }}}</strong> cm
+								@endif
+							</span>
+							<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Change</span></a>
+						@else
+							<span class="list-value"></span>
+							<a class="underlined" href="/orders/{{{ $order->id }}}/fit/{{{ $measurement }}}" title=""><span>Add</span></a>
+						@endif
+					</li>
+				@endif
+			@endforeach
+		</ul>
+	</section>
 
