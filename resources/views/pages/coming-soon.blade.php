1aaa67d1bc74d421789b10198210ebfa2d85b5ae
Dima Markus <dmi3000@gmail.com>
2015-11-12 15:31:43 -0500

    Add hidden links to coming soon page

--- resources/views/pages/coming-soon.blade.php
+++ resources/views/pages/coming-soon.blade.php
@@ -14,5 +14,16 @@
 		<br><br>
 		<h1 class="thin">Coming Soon</h1>
 	</div>
+	<div style="visibility: hidden;">
+    <a href="https://www.sapayol.com/"></a>
+    <a href="https://www.sapayol.com/jackets">jackets</a>
+    <a href="https://www.sapayol.com/jackets/bleecker">jackets/bleecker</a>
+    <a href="https://www.sapayol.com/jackets/bleecker/look">jackets/bleecker/look</a>
+    <a href="https://www.sapayol.com/how-it-works">how-it-works</a>
+    <a href="https://www.sapayol.com/who-we-are">who-we-are</a>
+    <a href="https://www.sapayol.com/our-leather">our-leather</a>
+    <a href="https://www.sapayol.com/login">login</a>
+    <a href="https://www.sapayol.com/terms">Terms</a>
+	</div>
 @stop
 
