30986bd15b1227d1051aa898787d277bf48f887b
Dima Markus <dmi3000@gmail.com>
2015-11-12 18:44:22 -0500

    Add email template

--- resources/views/emails/after-delivery.blade.php
+++ resources/views/emails/after-delivery.blade.php
@@ -4,16 +4,39 @@
 	About the {{{ $order->jacket->name }}} you just received
 @stop
 
-@section('main')
+@section('row1')
 	Hi {{{ $order->user->name }}},
-	We hope that you already had the time to try on your custom-tailored {{{ $order->jacket->name }}}.
-	How does it feel?
-	We would love to hear from you, since we don’t get to see your reaction when you put on your jacket.
-	Any feedback, a simple line or a picture would help us in our commitment to guarantee your satisfaction and our desire to always improve.
-	We will soon offer a flight jacket and a moto. Sign up here if you would like to be one of the first to know about them.
-	Put your {{{ $order->jacket->name }}} on a nice, wide hanger when you’re not wearing it.
-	LINK TO CARE INFORMATION
+@stop
 
-	SAPAYOL is based on the concept of doing things your way, of getting off the beaten path, and exploring new ideas or places. Our jackets are designed to be your companion on that road.
-	In that sense, go on, be special. Because regular doesn’t fit you.
+@section('row2')
+	<p>
+		We hope that you already had the time to try on your custom-tailored {{{ $order->jacket->name }}}.<br>
+		How does it feel?<br>
+	</p>
+	<p>
+		We would love to hear from you, since we don’t get to see your reaction when you put on your jacket.
+		Any feedback, a simple line or a picture would help us in our commitment to guarantee your satisfaction and our desire to always improve. You can reply directly to this email if you want to reach out.
+	</p>
+	<p>
+		Put your {{{ $order->jacket->name }}} on a nice, wide hanger when you’re not wearing it.<br>
+		<a href="#">How to care for your jacket</a>
+	</p>
+@stop
+
+@section('row3')
+	<p>We will soon offer a flight jacket and a moto. Sign up if you would like to be one of the first to know about them.</p>
+	<br>
+	<div class="text-center">
+		<a href="{{{ env('SITE_URL') }}}/jackets?jacket-updates" class="button">Sign Up For Updates</a>
+	</div>
+@stop
+
+@section('row4')
+	<p>
+		<br>
+		<br>
+		<br>
+		SAPAYOL is based on the concept of doing things your way, of getting off the beaten path, and exploring new ideas or 	places. Our jackets are designed to be your companion on that road.
+		In that sense, go on, be special. <em>Because regular doesn’t fit you.</em>
+	</p>
 @stop
--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,28 +1,79 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>@yield('title')</title>
-    <style>
-			body {
-				font-family: 'HelveticaNeue', 'Helvetica', 'Arial', 'sans-serif';
-			}
-			small {
-				font-size: 70%;
-			}
-			table caption {
-				font-size: 18px;
-				text-align: left;
-				margin-bottom: 16px;
-			}
-			.value-list-table td {
-				padding: 4px 0;
-			}
+<head>
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>@yield('title')</title>
+
+    <style type="text/css">
+      @include('partials.emails.email-css');
     </style>
-  </head>
-  <body>
 
-		@yield('main');
+</head>
 
-	</body>
+<body itemscope itemtype="http://schema.org/EmailMessage">
+
+<table class="body-wrap">
+	<tr>
+		<td></td>
+
+		<td class="container" width="600">
+			<div class="content">
+				<table class="main" width="100%" cellpadding="0" cellspacing="0">
+					<tr>
+						<td class="content-wrap">
+							<table width="100%" cellpadding="0" cellspacing="0">
+								<tr>
+									<td class="content-block">
+										<a href="{{{ env('SITE_URL') }}}"><img src="https://sapayol.com/images/logo-black.svg" alt="SAPAYOL Logo" class="logo"></a>
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row1')
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row2')
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row3')
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row4')
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row5')
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block">
+										@yield('row5')
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+				<div class="footer">
+					<table width="100%">
+						<tr>
+							<td class="aligncenter content-block">@yield('footer')</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</td>
+		<td></td>
+	</tr>
+</table>
+
+</body>
 </html>
