d36d366088c2c7f8acb51176ef6e63dba55a3592
Dima Markus <dmi3000@gmail.com>
2015-11-16 14:45:19 -0500

    Add price info on jacket page

--- resources/views/partials/jacket/summary.blade.php
+++ resources/views/partials/jacket/summary.blade.php
@@ -1,10 +1,11 @@
 <section class="small-12 medium-9 large-6 medium-centered columns jacket-summary">
 	<h1 class="with-subheading">{{{ $jacket->name }}}</h1>
-	<span class="thin large-price"><small>US </small> ${{{ number_format($jacket->price) }}}</span>
 	<p>
 		<br>
 		Perfecto&ndash;style biker leather jacket, made of black, heavy lamb leather. You can customize the hardware and lining color.
 	</p>
+	<span class="thin large-price"><small>US </small> ${{{ number_format($jacket->price) }}}</span><br>
+	<em>Free shipping worldwide<br>Taxes and duties included</em>
 	<div class="text-center">
 		<a href="{!! route('jackets.look', $jacket->model) !!}" class="button expand-on-small animated slideInUp call-to-action">Design Yours Now</a>
 	</div>
