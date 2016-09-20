5b1d8234db2078f31b63eb6b38ccb82ded95b31d
Dima Markus <dmi3000@gmail.com>
2016-09-19 22:13:52 -0400

    Remove elements from jacket summary

--- resources/assets/js/controllers/jacketController.js
+++ resources/assets/js/controllers/jacketController.js
@@ -2,26 +2,29 @@ var jacketController = angular.module('jacketController', []);
 
 jacketController.controller('jacketCtrl', ['$scope', function($scope) {
 
-  $(document).ready(function($){
-    // var trigger = $('.stick-to-bottom').position().top - 128;
-    var trigger = $('.stick-to-bottom').position().top + 500;
-    var stickyButtonBottom = $('.stick-to-bottom').offset().top - 160;
-    var stickyNav = function(){
-      var scrollTop = $(window).scrollTop();
-      if (scrollTop >= trigger) {
-          $('.stick-to-bottom').show();
-          $('.stick-to-bottom').addClass('sticky');
-      } else {
-          $('.stick-to-bottom').hide();
-          $('.stick-to-bottom').removeClass('sticky');
-      }
-    };
+  // $(document).ready(function($){
+  //   // var trigger = $('.stick-to-bottom').position().top - 128;
+  //   if (!$('.stick-to-bottom')) {
+  //     return
+  //   }
+  //   var trigger = $('.stick-to-bottom').position().top + 500;
+  //   var stickyButtonBottom = $('.stick-to-bottom').offset().top - 160;
+  //   var stickyNav = function(){
+  //     var scrollTop = $(window).scrollTop();
+  //     if (scrollTop >= trigger) {
+  //         $('.stick-to-bottom').show();
+  //         $('.stick-to-bottom').addClass('sticky');
+  //     } else {
+  //         $('.stick-to-bottom').hide();
+  //         $('.stick-to-bottom').removeClass('sticky');
+  //     }
+  //   };
 
-    stickyNav();
+  //   stickyNav();
 
-    $(window).scroll(function() {
-        stickyNav();
-    });
-  });
+  //   $(window).scroll(function() {
+  //       stickyNav();
+  //   });
+  // });
 
 }]);
