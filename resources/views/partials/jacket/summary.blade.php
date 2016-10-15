c291ff2bc62471061f3a61f6c453e4e3a21c1696
Dima Markus <dmi3000@gmail.com>
2016-10-15 17:36:06 -0400

    Change jacket copy

--- resources/views/partials/jacket/summary.blade.php
+++ resources/views/partials/jacket/summary.blade.php
@@ -1,5 +1,5 @@
 <section class="small-12 medium-9 large-6 medium-centered columns jacket-summary">
 	<h1 class="with-subheading">{{{ $jacket->name }}}</h1>
 	<span class="thin large-price"><small>US </small> ${{{ number_format($jacket->price) }}}</span><br>
-	<em>Free shipping worldwide<br>Taxes and duties included</em>
+	<em>Free shipping worldwide. Taxes and duties included.</em>
 </section>
