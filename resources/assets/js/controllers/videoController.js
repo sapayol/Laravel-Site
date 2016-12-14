6aabdffd71fe12fefc8f5e27b9900055e0513ed8
Dima Markus <dmi3000@gmail.com>
2016-12-13 21:42:13 -0500

    Add play button and volume to home video

--- resources/assets/js/controllers/videoController.js
+++ resources/assets/js/controllers/videoController.js
@@ -1,28 +1,32 @@
 var videoController = angular.module('videoController', []);
 
 videoController.controller('videoCtrl', ['$scope', function($scope) {
-		$(document).ready(function(){
-	  	plyr.setup({
-    		controls: ["play", "fullscreen"]
-		  });
-	    var media = document.querySelectorAll(".player")[0].plyr.media;
-	    var playerControls = $(".player-controls");
-	    media.addEventListener("playing", function() {
-	      playerControls.addClass('descended');
-	    });
-
-	    // This script gets the svg icons that are the video player controls
-      var a = new XMLHttpRequest(),
-	        b = document.body;
-        a.open("GET", "https://cdn.plyr.io/1.3.3/sprite.svg", true);
-        a.send();
-        a.onload = function(){
-          var c = document.createElement("div");
-          c.style.display = "none";
-          c.innerHTML = a.responseText;
-          b.insertBefore(c, b.childNodes[0]);
-        }
+	$(document).ready(function(){
+  	plyr.setup({
+  		controls: ["play", "fullscreen", "volume"]
 	  });
+    var media = document.querySelectorAll(".player")[0].plyr.media;
+    var playerControls = $(".player-controls");
+    media.addEventListener("playing", function() {
+      playerControls.addClass('descended');
+    });
+
+    $scope.onPlayClick = function () {
+      media.play()
+    }
+
+    // This script gets the svg icons that are the video player controls
+    var a = new XMLHttpRequest(),
+        b = document.body;
+      a.open("GET", "https://cdn.plyr.io/1.3.3/sprite.svg", true);
+      a.send();
+      a.onload = function(){
+        var c = document.createElement("div");
+        c.style.display = "none";
+        c.innerHTML = a.responseText;
+        b.insertBefore(c, b.childNodes[0]);
+      }
+  });
 
 }]);
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,6 +21,9 @@ SAPAYOL offers made-to-measure, customizable leather jackets, tailored by master
 			    <source src="/videos/jackets/{{ $randomModel }}.mp4"  type="video/mp4">
 			    <a href="/videos/jackets/{{ $randomModel }}.mp4">Download</a>
 			  </video>
+			  <div class="overlay">
+			  	<button ng-click='onPlayClick()' class="play-button"></button>
+			  </div>
 			</div>
 			<div class="title-container">
 				<h2 class="hero-text">
