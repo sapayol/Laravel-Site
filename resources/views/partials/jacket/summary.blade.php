3348e1a4cf7b100fa0aeb0f875cb4e64ea899db8
Dima Markus <dmi3000@gmail.com>
2017-01-11 23:47:34 -0500

    Update jacket summary

--- resources/views/partials/jacket/summary.blade.php
+++ resources/views/partials/jacket/summary.blade.php
@@ -1,5 +1,12 @@
 <section class="small-12 medium-9 large-6 medium-centered columns jacket-summary">
-	<h1 class="with-subheading">{{{ $jacket->name }}}</h1>
-	<span class="thin large-price"><small>US </small> ${{{ number_format($jacket->price) }}}</span><br>
+	<h1 class="with-subheading">
+    {{{ $jacket->name }}}
+    @if ($jacket->collar_colors()->count() > 0)
+      <small>with detachable merino wool collar</small>
+    @endif
+  </h1>
+  <br>
+	<span class="thin large-price"><small>US </small> ${{{ number_format($jacket->price) }}}</span>
+  <br>
 	<em>Free shipping worldwide. Taxes and duties included.</em>
 </section>
