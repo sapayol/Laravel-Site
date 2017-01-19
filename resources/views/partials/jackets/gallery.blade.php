deb4ac805760106d62049c4a91d21237785d0a42
Dima Markus <dmi3000@gmail.com>
2017-01-19 15:50:14 -0500

    Make home page jacket grid 2x2

--- resources/views/partials/jackets/gallery.blade.php
+++ resources/views/partials/jackets/gallery.blade.php
@@ -1,5 +1,5 @@
 <article id="jackets" class="large-12 medium-12 small-12 columns text-center">
-	<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
+	<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2 x-large-block-grid-4">
 		@foreach ($jackets as $jacket)
 		  <li class="text-center">
 				@if ($jacket->active)
