1f42080eea7730415ca72d87999cad196fb4b31d
Dima Markus <dmi3000@gmail.com>
2017-02-17 21:15:27 -0500

    Fix checkout image

--- resources/views/partials/checkout/order-summary.blade.php
+++ resources/views/partials/checkout/order-summary.blade.php
@@ -1,8 +1,18 @@
 <?php
-	$imageName = $order->lining_color()->name . '-' . $order->hardware_color()->name;
+	$jacket = $order->jacket;
+	$lining_color = $order->lining_color()->name;
+	$lining_color = $lining_color === 'bordeaux' ? 'red' : $lining_color;
+	$hardware_color = $order->hardware_color()->name;
+	$hardware_color = $hardware_color === 'graphite' ? 'gray' : $hardware_color;
+  if ($jacket->model === 'linden') {
+    $collar_color = ($order->collar_color() === '14' ? 'black' : 'gray');
+    $front_image = $lining_color . '-' . $hardware_color . '-' . $collar_color;
+  } else {
+    $front_image = $lining_color . '-' . $hardware_color;
+  }
 ?>
 
-	<img class="customization-image" src="/images/photos/jackets/{{{ $order->jacket->model }}}/variations/{{{ $imageName }}}-medium.jpg" alt="Jacket Photo">
+	<img class="customization-image" src="/images/photos/jackets/{{{ $order->jacket->model }}}/variations/{{{ $front_image }}}-medium.jpg" alt="Jacket Photo">
 
 	<section class="large-6 medium-6 small-12 columns">
 		<h3 class="thin left">Look</h3>
