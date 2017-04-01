696d717a6475ff3a9363e81367e6b71037f0b85a
Dima Markus <dmi3000@gmail.com>
2017-03-31 20:13:36 -0400

    Fix spacing in email

--- resources/views/partials/emails/payment-info.blade.php
+++ resources/views/partials/emails/payment-info.blade.php
@@ -21,7 +21,7 @@
 				{{-- TODO - Make list key dynamic depending on attribute --}}
 				<small class="list-key">{{{ $attribute_map[$attribute->type] }}}</small>
 				<span class="list-value line-item-price">
-					<span><small>$ </small>{{{ $attribute->price }}}</span>
+					<span><small>$ </small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{{ $attribute->price }}}</span>
 				</span>
 			</li>
 		@endif
