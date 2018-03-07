ab266f3226dc0b7a1c702f5cef7b76e3c30b083a
Dima Markus <dmi3000@gmail.com>
2018-03-07 15:59:40 -0500

    Make jacket page respond to hash changes

--- resources/assets/js/controllers/jacketController.js
+++ resources/assets/js/controllers/jacketController.js
@@ -2,14 +2,37 @@ var jacketController = angular.module('jacketController', []);
 
 jacketController.controller('jacketCtrl', ['$scope', '$http', '$q', 'Session', '$timeout', 'Auth', 'notifyUser', function($scope, $http, $q, Session, $timeout, Auth, notifyUser) {
 
+
+  var getHashColor = function() {
+    if (window.location.hash) {
+      const hash = window.location.hash.substring(1)
+      const validHash = hash === 'black' || hash === 'brown'
+
+      return validHash ? hash : false
+    } else {
+      return false
+    }
+  }
+
+  getColorId = function(name) {
+    return name === 'black' ? '1' : '2';
+  }
+
   $scope.init = function(model) {
     $scope.jacket = typeof(sapayol.session[model]) !== 'undefined' ? sapayol.session[model] : sapayol.jackets[model];
-    $scope.leather_color = $scope.jacket ? $scope.jacket.leather_color : '1'
+    var hashColor = getHashColor()
+    if (hashColor) {
+      $scope.leather_color = getColorId(hashColor)
+      $scope.jacket.leather_color = getColorId(hashColor)
+    } else {
+      $scope.leather_color = $scope.jacket ? $scope.jacket.leather_color : '1'
+    }
   }
 
   $scope.getColorName = function(id) {
     return id === '1' ? 'black' : 'brown';
   }
+
 
   // Update the session on the server to match the changes that the user makes
   $scope.updateSessionCache = function() {
