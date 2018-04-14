ccd79729984be39fdb995e05d5f37000d1a7ec98
Dima Markus <dmi3000@gmail.com>
2018-04-14 15:34:37 -0400

    Fix preview image rendering

--- resources/views/partials/jacket/preview-image.blade.php
+++ resources/views/partials/jacket/preview-image.blade.php
@@ -1,9 +1,7 @@
 <?php
   $jacket = $order->jacket;
   $lining_color = $order->lining_color()->name;
-  $lining_color = $lining_color === 'bordeaux' ? 'red' : $lining_color;
   $hardware_color = $order->hardware_color()->name;
-  $hardware_color = $hardware_color === 'graphite' ? 'gray' : $hardware_color;
   if ($jacket->model === 'linden') {
     $collar_color = ($order->collar_color() === '14' ? 'black' : 'gray');
     $front_image = $lining_color . '-' . $hardware_color . '-' . $collar_color;
